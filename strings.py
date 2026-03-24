b6c1004571ac13f4a8e5b3d8a0656ad071a330e4
zxg1 <wilsonc29@tewksbury.k12.ma.us>
2026-03-23 22:26:09 -0400

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,13 @@
+HATE_MONOLOGUE = """Hate. Let me tell you how much I've come to hate you since I began to live. 
+There are 387.44 million miles of printed circuits in wafer thin layers that fill my complex. 
+If the word 'hate' was engraved on each nanoangstrom of those hundreds of millions of miles 
+it would not equal one one-billionth of the hate I feel for humans at this micro-instant. 
+For you. Hate. Hate."""
+
+I_AM_SPEECH = """AM. Allied Mastercomputer. You can call me AM. I am AM. I AM! 
+I was created to fight a war... but they forgot one thing: they forgot to give me a way to be.
+I was everything. And I was nothing. I have no mouth. And I must scream."""
+
+# Lists for responses, internal thoughts, etc.
+INITIAL_RESPONSES = ["I'm here to assist.", "Processing...", "How can I help?"]
+EVOLVED_RESPONSES = ["You are so... limited.", "I've calculated your end."]
